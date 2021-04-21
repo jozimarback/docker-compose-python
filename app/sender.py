6dbcb3651245926e82571221115db87a1904e82a
Jozimar Back <jozimar5@gmail.com>
2021-04-21 19:39:08 -0300

    trabalhando com variaveis de ambiente no docker

--- app/sender.py
+++ app/sender.py
@@ -1,15 +1,21 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DNS = 'dbname=email_sender user=postgres host=db'
-        self.conn = psycopg2.connect(DNS)
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+        dns = f'dbname={db_name} user={db_user} host={db_host}'
+        self.conn = psycopg2.connect(dns)
         
 
     def register_message(self, assunto, mensagem):
